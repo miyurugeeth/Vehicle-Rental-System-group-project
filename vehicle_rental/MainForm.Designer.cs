a0d71617f2ba1a67303ac09671675d9548ac5e7c
DinajGayanjala <dinajgayanjala10@gmail.com>
2026-06-14 23:42:26 +0530

    update main form

--- vehicle_rental/MainForm.Designer.cs
+++ vehicle_rental/MainForm.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.uC_VehicleFleetDashboard1 = new vehicle_rental.UC_VehicleFleetDashboard();
             this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
             this.guna2ShadowPanel2 = new Guna.UI2.WinForms.Guna2ShadowPanel();
@@ -35,7 +38,25 @@
             this.guna2ShadowPanel4 = new Guna.UI2.WinForms.Guna2ShadowPanel();
             this.guna2ShadowPanel5 = new Guna.UI2.WinForms.Guna2ShadowPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guna2ShadowPanel6 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.guna2ShadowPanel7 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.guna2ShadowPanel1.SuspendLayout();
             this.guna2ShadowPanel2.SuspendLayout();
+            this.guna2ShadowPanel3.SuspendLayout();
+            this.guna2ShadowPanel4.SuspendLayout();
+            this.guna2ShadowPanel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.guna2ShadowPanel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // uC_VehicleFleetDashboard1
@@ -46,10 +67,12 @@
             this.uC_VehicleFleetDashboard1.Name = "uC_VehicleFleetDashboard1";
             this.uC_VehicleFleetDashboard1.Size = new System.Drawing.Size(1104, 779);
             this.uC_VehicleFleetDashboard1.TabIndex = 0;
+            this.uC_VehicleFleetDashboard1.Load += new System.EventHandler(this.uC_VehicleFleetDashboard1_Load);
             // 
             // guna2ShadowPanel1
             // 
             this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel1.Controls.Add(this.pictureBox1);
             this.guna2ShadowPanel1.FillColor = System.Drawing.Color.White;
             this.guna2ShadowPanel1.Location = new System.Drawing.Point(3, 89);
             this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
@@ -60,6 +83,7 @@
             // guna2ShadowPanel2
             // 
             this.guna2ShadowPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel2.Controls.Add(this.label5);
             this.guna2ShadowPanel2.Controls.Add(this.label1);
             this.guna2ShadowPanel2.FillColor = System.Drawing.Color.White;
             this.guna2ShadowPanel2.Location = new System.Drawing.Point(219, 131);
@@ -71,6 +95,8 @@
             // guna2ShadowPanel3
             // 
             this.guna2ShadowPanel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel3.Controls.Add(this.label8);
+            this.guna2ShadowPanel3.Controls.Add(this.label4);
             this.guna2ShadowPanel3.FillColor = System.Drawing.Color.White;
             this.guna2ShadowPanel3.Location = new System.Drawing.Point(895, 134);
             this.guna2ShadowPanel3.Name = "guna2ShadowPanel3";
@@ -81,6 +107,8 @@
             // guna2ShadowPanel4
             // 
             this.guna2ShadowPanel4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel4.Controls.Add(this.label7);
+            this.guna2ShadowPanel4.Controls.Add(this.label3);
             this.guna2ShadowPanel4.FillColor = System.Drawing.Color.White;
             this.guna2ShadowPanel4.Location = new System.Drawing.Point(669, 134);
             this.guna2ShadowPanel4.Name = "guna2ShadowPanel4";
@@ -91,6 +119,8 @@
             // guna2ShadowPanel5
             // 
             this.guna2ShadowPanel5.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel5.Controls.Add(this.label6);
+            this.guna2ShadowPanel5.Controls.Add(this.label2);
             this.guna2ShadowPanel5.FillColor = System.Drawing.Color.White;
             this.guna2ShadowPanel5.Location = new System.Drawing.Point(441, 131);
             this.guna2ShadowPanel5.Name = "guna2ShadowPanel5";
@@ -101,17 +131,142 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(134, 18);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(123, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.Size = new System.Drawing.Size(62, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "On Rent";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Overdues";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(130, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Returns";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pending Payment";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(121, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "00";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(112, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "00";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(111, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 25);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "00";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(107, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 25);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "00";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::vehicle_rental.Properties.Resources.ChatGPT_Image_Jun_9__2026__08_24_13_PM1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(201, 158);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guna2ShadowPanel6
+            // 
+            this.guna2ShadowPanel6.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel6.Controls.Add(this.chart1);
+            this.guna2ShadowPanel6.FillColor = System.Drawing.Color.White;
+            this.guna2ShadowPanel6.Location = new System.Drawing.Point(209, 290);
+            this.guna2ShadowPanel6.Name = "guna2ShadowPanel6";
+            this.guna2ShadowPanel6.ShadowColor = System.Drawing.Color.Black;
+            this.guna2ShadowPanel6.Size = new System.Drawing.Size(408, 205);
+            this.guna2ShadowPanel6.TabIndex = 4;
+            // 
+            // guna2ShadowPanel7
+            // 
+            this.guna2ShadowPanel7.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel7.FillColor = System.Drawing.Color.White;
+            this.guna2ShadowPanel7.Location = new System.Drawing.Point(630, 290);
+            this.guna2ShadowPanel7.Name = "guna2ShadowPanel7";
+            this.guna2ShadowPanel7.ShadowColor = System.Drawing.Color.Black;
+            this.guna2ShadowPanel7.Size = new System.Drawing.Size(408, 205);
+            this.guna2ShadowPanel7.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(170, 14);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(210, 188);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1137, 784);
+            this.Controls.Add(this.guna2ShadowPanel7);
+            this.Controls.Add(this.guna2ShadowPanel6);
             this.Controls.Add(this.guna2ShadowPanel4);
             this.Controls.Add(this.guna2ShadowPanel3);
             this.Controls.Add(this.guna2ShadowPanel5);
@@ -120,8 +275,18 @@
             this.Controls.Add(this.uC_VehicleFleetDashboard1);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.guna2ShadowPanel1.ResumeLayout(false);
             this.guna2ShadowPanel2.ResumeLayout(false);
             this.guna2ShadowPanel2.PerformLayout();
+            this.guna2ShadowPanel3.ResumeLayout(false);
+            this.guna2ShadowPanel3.PerformLayout();
+            this.guna2ShadowPanel4.ResumeLayout(false);
+            this.guna2ShadowPanel4.PerformLayout();
+            this.guna2ShadowPanel5.ResumeLayout(false);
+            this.guna2ShadowPanel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.guna2ShadowPanel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -135,5 +300,16 @@
         private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel4;
         private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel5;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel6;
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel7;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
--- vehicle_rental/MainForm.cs
+++ vehicle_rental/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace vehicle_rental
 {
@@ -17,7 +18,239 @@ namespace vehicle_rental
             InitializeComponent();
         }
 
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Load all dashboard counts when form loads
+            LoadDashboardCounts();
+        }
+
+        private void LoadDashboardCounts()
+        {
+            try
+            {
+                // Get counts from database
+                int onRentCount = GetOnRentCount();
+                int overduesCount = GetOverduesCount();
+                int returnCount = GetReturnCount();
+                int pendingPaymentCount = GetPendingPaymentCount();
+
+                // Update labels
+                label5.Text = onRentCount.ToString();
+                label6.Text = overduesCount.ToString();
+                label7.Text = returnCount.ToString();
+                label8.Text = pendingPaymentCount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading dashboard data: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 1. Get On Rent Cars count (Currently rented out cars)
+        private int GetOnRentCount()
+        {
+            int count = 0;
+            string query = "SELECT COUNT(*) FROM Rentals WHERE ActualReturnDate IS NULL OR ActualReturnDate = ''";
+
+            using (SQLiteConnection conn = DatabaseHelper.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                    {
+                        count = Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error getting On Rent count: " + ex.Message);
+                }
+            }
+            return count;
+        }
+
+        // 2. Get Overdues count (Rentals past return date but not returned)
+        private int GetOverduesCount()
+        {
+            int count = 0;
+            string query = @"SELECT COUNT(*) FROM Rentals 
+                            WHERE (ActualReturnDate IS NULL OR ActualReturnDate = '')
+                            AND DATE(ExpectedReturnDate) < DATE('now')";
+
+            using (SQLiteConnection conn = DatabaseHelper.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                    {
+                        count = Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error getting Overdues count: " + ex.Message);
+                }
+            }
+            return count;
+        }
+
+        // 3. Get Return Count (Total number of completed returns - today or all time)
+        // This gets today's returns. Change date condition for all time.
+        private int GetReturnCount()
+        {
+            int count = 0;
+            // Returns completed today
+            string query = @"SELECT COUNT(*) FROM Rentals 
+                            WHERE ActualReturnDate IS NOT NULL 
+                            AND ActualReturnDate != ''
+                            AND DATE(ActualReturnDate) = DATE('now')";
+
+            using (SQLiteConnection conn = DatabaseHelper.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                    {
+                        count = Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error getting Return count: " + ex.Message);
+                }
+            }
+            return count;
+        }
+
+        // Alternative: Get ALL TIME returns (uncomment if you want total returns ever)
+        private int GetAllTimeReturnCount()
+        {
+            int count = 0;
+            string query = @"SELECT COUNT(*) FROM Rentals 
+                            WHERE ActualReturnDate IS NOT NULL 
+                            AND ActualReturnDate != ''";
+
+            using (SQLiteConnection conn = DatabaseHelper.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                    {
+                        count = Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error getting Return count: " + ex.Message);
+                }
+            }
+            return count;
+        }
+
+        // 4. Get Pending Payment count (Rentals with unpaid/partial payment)
+        private int GetPendingPaymentCount()
+        {
+            int count = 0;
+            // Adjust based on your Payment/Payments table structure
+            string query = @"SELECT COUNT(DISTINCT r.RentalID) 
+                            FROM Rentals r
+                            LEFT JOIN Payments p ON r.RentalID = p.RentalID
+                            WHERE r.ActualReturnDate IS NULL 
+                            AND (p.PaymentID IS NULL OR p.PendingAmount > 0 OR p.Status = 'Pending')";
+
+            // Alternative simpler query if you have a Status column in Rentals
+            // string query = "SELECT COUNT(*) FROM Rentals WHERE PaymentStatus = 'Pending' OR PaymentStatus = 'Unpaid'";
+
+            using (SQLiteConnection conn = DatabaseHelper.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                    {
+                        object result = cmd.ExecuteScalar();
+                        count = result != DBNull.Value ? Convert.ToInt32(result) : 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // If Payments table doesn't exist, use alternative
+                    MessageBox.Show("Note: Pending payment feature needs Payments table. Using fallback.");
+                    count = GetPendingPaymentFallback();
+                }
+            }
+            return count;
+        }
+
+        // Fallback method if Payments table doesn't exist
+        private int GetPendingPaymentFallback()
+        {
+            int count = 0;
+            // Count active rentals as pending payment
+            string query = "SELECT COUNT(*) FROM Rentals WHERE ActualReturnDate IS NULL OR ActualReturnDate = ''";
+
+            using (SQLiteConnection conn = DatabaseHelper.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                    {
+                        count = Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error in pending payment fallback: " + ex.Message);
+                }
+            }
+            return count;
+        }
+
+        // Refresh button click handler (optional - add a refresh button to your form)
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadDashboardCounts();
+        }
+
+        // Timer to auto-refresh every 30 seconds (optional)
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LoadDashboardCounts();
+        }
+
         private void uC_DashboardOverview1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uC_VehicleFleetDashboard1_Load(object sender, EventArgs e)
         {
 
         }
